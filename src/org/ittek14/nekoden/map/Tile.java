1347b15493d42e2fd2fc252693ea42c854dfbf5c
Sebastian "Into" Klawon <intothenethermc@wp.pl>
2016-01-03 17:13:38 +0100

    Tile has 2 new constructors!

--- src/org/ittek14/nekoden/map/Tile.java
+++ src/org/ittek14/nekoden/map/Tile.java
@@ -1,5 +1,11 @@
 package org.ittek14.nekoden.map;
 
 public class Tile {
-
+	private String resourceID;
+	
+	public Tile() {}
+	
+	public Tile(String _resourceID) {
+		resourceID = _resourceID;
+	}
 }
