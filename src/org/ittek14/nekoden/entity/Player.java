286beaeb37a6cf700c96420c6e87b0f0dfe05b9b
Sebastian "Into" Klawon <intothenethermc@wp.pl>
2016-01-03 23:18:25 +0100

    Player class added

---
+++ src/org/ittek14/nekoden/entity/Player.java
@@ -0,0 +1,10 @@
+package org.ittek14.nekoden.entity;
+
+import org.newdawn.slick.geom.Vector2f;
+
+public class Player extends Entity {
+
+	public Player(Vector2f vector2f, String sprite) {
+		super(vector2f, sprite);
+	}
+}
