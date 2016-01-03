a38b00e371079006dd9924baf453cc11c2976e7d
TheMcDucky <TheMcDucky@hotmail.com>
2016-01-03 21:31:02 +0100

    animation stuff

--- src/org/ittek14/nekoden/resource/AnimationResource.java
+++ src/org/ittek14/nekoden/resource/AnimationResource.java
@@ -1,8 +1,25 @@
 package org.ittek14.nekoden.resource;
 
-public class AnimationResource extends Resource {
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
 
-	public AnimationResource(String id) {
+public class AnimationResource extends Resource {
+	private Image[] frames;
+	private int speed;
+	
+	public AnimationResource(String id, int speed) {
 		super(id);
+		this.speed = speed;
+	}
+	
+	public void loadFrames(String[] sprites){
+		frames = new Image[sprites.length];
+		for(int i = 0; i < frames.length; i++) {
+			frames[i] = ResourceManager.getImageResource(sprites[i]).getImage();
+		}
+	}
+	
+	public Animation getAnimation() {
+		return new Animation(frames, speed);
 	}
 }
