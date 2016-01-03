1f65fc1b7662fe6cdd3e8ec4ed01e24309752409
TheMcDucky <TheMcDucky@hotmail.com>
2016-01-03 17:10:05 +0100

    added stuff

--- src/org/ittek14/nekoden/map/Map.java
+++ src/org/ittek14/nekoden/map/Map.java
@@ -1,12 +1,28 @@
 package org.ittek14.nekoden.map;
 
+import java.util.ArrayList;
+
+import org.ittek14.nekoden.entity.Entity;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
 public class Map {
 	public int width, height;
 	public Tile[][] tiles;
+	public ArrayList<Entity> entities = new ArrayList<Entity>();
 	
 	public Map(int w, int h){
 		this.width = w; 
 		this.height = h;
 		tiles = new Tile[3][w*h];
+	}
+	
+	public void update(GameContainer container, StateBasedGame game, int delta) {
+		
+	}
+	
+	public void render(GameContainer container, StateBasedGame game, Graphics g) {
+		
 	}
 }
