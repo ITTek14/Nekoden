75f4e3954caa1204f0ed3f2da7c538f5891cec86
TheMcDucky <TheMcDucky@hotmail.com>
2016-01-03 18:03:39 +0100

    posh

--- src/org/ittek14/nekoden/resource/ResourceManager.java
+++ src/org/ittek14/nekoden/resource/ResourceManager.java
@@ -2,12 +2,31 @@ package org.ittek14.nekoden.resource;
 
 import java.util.ArrayList;
 
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.util.xml.XMLElement;
+import org.newdawn.slick.util.xml.XMLElementList;
+import org.newdawn.slick.util.xml.XMLParser;
+
 public class ResourceManager {
 	
 	public static ArrayList<Resource> resources = new ArrayList<>();
 	
 	public ResourceManager() {
 		resources.add(new ImageResource("resources/sprite.png"));
+	}
+	
+	public void loadResourcePack(String path) {
+		XMLParser parser = new XMLParser();
+		XMLElement origin;
+		try {
+			origin = parser.parse(path);
+			XMLElementList imgElements = origin.getChildrenByName("IMG");
+			for(int i = 0; i < imgElements.size(); i++) {
+				ImageResource res = new ImageResource(imgElements.get(i).getAttribute("id"));
+			}
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Resource getResource(String id) {
