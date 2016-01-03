c83406f80cee67e39bf9f5d91c368ad5743f712f
Sebastian "Into" Klawon <intothenethermc@wp.pl>
2016-01-03 23:31:47 +0100

    Player class overrides update

--- src/org/ittek14/nekoden/entity/Player.java
+++ src/org/ittek14/nekoden/entity/Player.java
@@ -1,10 +1,29 @@
 package org.ittek14.nekoden.entity;
 
+import org.lwjgl.input.Keyboard;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.StateBasedGame;
 
 public class Player extends Entity {
 
 	public Player(Vector2f vector2f, String sprite) {
 		super(vector2f, sprite);
+	}
+	
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) {
+		if(Keyboard.isKeyDown(Keyboard.KEY_W))  {
+			
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_S))  {
+			
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_A))  {
+			
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_D))  {
+			
+		}
 	}
 }
