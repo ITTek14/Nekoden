9db52d40885a4b9323466f524eca7c223fc6691c
TheMcDucky <TheMcDucky@hotmail.com>
2016-01-06 19:16:19 +0100

    changed map format, which broke map loading for now

---
+++ resources/tiles/terrain.tsx
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Terrain" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="terrain.png" width="960" height="960"/>
+ <terraintypes>
+  <terrain name="Water" tile="5"/>
+  <terrain name="Grass" tile="6"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,0">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="60" duration="300"/>
+   <frame tileid="120" duration="300"/>
+   <frame tileid="60" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1" terrain="1,1,0,1">
+  <animation>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="61" duration="300"/>
+   <frame tileid="121" duration="300"/>
+   <frame tileid="61" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="2" terrain="0,0,0,1">
+  <animation>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="62" duration="300"/>
+   <frame tileid="122" duration="300"/>
+   <frame tileid="62" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="3" terrain="0,0,1,0">
+  <animation>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="63" duration="300"/>
+   <frame tileid="123" duration="300"/>
+   <frame tileid="63" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="4" terrain="1,1,0,0">
+  <animation>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="64" duration="300"/>
+   <frame tileid="124" duration="300"/>
+   <frame tileid="64" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="5" terrain="0,0,0,0">
+  <animation>
+   <frame tileid="5" duration="300"/>
+   <frame tileid="65" duration="300"/>
+   <frame tileid="125" duration="300"/>
+   <frame tileid="65" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="6" terrain="1,1,1,1">
+  <animation>
+   <frame tileid="6" duration="300"/>
+   <frame tileid="66" duration="300"/>
+   <frame tileid="126" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="30" terrain="1,0,1,1">
+  <animation>
+   <frame tileid="30" duration="300"/>
+   <frame tileid="90" duration="300"/>
+   <frame tileid="150" duration="300"/>
+   <frame tileid="90" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="31" terrain="0,1,1,1">
+  <animation>
+   <frame tileid="31" duration="300"/>
+   <frame tileid="91" duration="300"/>
+   <frame tileid="151" duration="300"/>
+   <frame tileid="91" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="32" terrain="0,1,0,0">
+  <animation>
+   <frame tileid="32" duration="300"/>
+   <frame tileid="92" duration="300"/>
+   <frame tileid="152" duration="300"/>
+   <frame tileid="92" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="33" terrain="1,0,0,0">
+  <animation>
+   <frame tileid="33" duration="300"/>
+   <frame tileid="93" duration="300"/>
+   <frame tileid="153" duration="300"/>
+   <frame tileid="93" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="34" terrain="0,0,1,1">
+  <animation>
+   <frame tileid="34" duration="300"/>
+   <frame tileid="94" duration="300"/>
+   <frame tileid="154" duration="300"/>
+   <frame tileid="94" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="35" terrain="1,0,1,0">
+  <animation>
+   <frame tileid="35" duration="300"/>
+   <frame tileid="95" duration="300"/>
+   <frame tileid="155" duration="300"/>
+   <frame tileid="95" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="36" terrain="0,1,0,1">
+  <animation>
+   <frame tileid="36" duration="300"/>
+   <frame tileid="96" duration="300"/>
+   <frame tileid="156" duration="300"/>
+   <frame tileid="96" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
